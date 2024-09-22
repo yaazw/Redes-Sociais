2048a7345b971ce2db1b3dc28b18ac7244abfa3e
yaazw <126116974+yaazw@users.noreply.github.com>
2024-09-22 15:51:04 -0300

    update

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,3 +1,5 @@
+import { getCSS } from "./common.js"
+
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
@@ -9,14 +11,23 @@ async function quantidadeUsuariosPorRede() {
         {
             x: nomeDasRedes, 
             y: quantidadeDeUsuarios, 
-            type: 'bar'
+            type: 'bar',
+            marker: {
+                color: getCSS('--primary-color')
+            }
         }
     ]
+
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color')
+
+    }
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data)
+    Plotly.newPlot(grafico, data, layout)
 }
 
 quantidadeUsuariosPorRede()
