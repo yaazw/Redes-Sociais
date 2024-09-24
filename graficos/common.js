11f5f69c125ecd8c24255666c2c0d1c34cc1635a
yasmin <126116974+yaazw@users.noreply.github.com>
2024-09-24 19:57:11 -0300

    Update common.js

--- graficos/common.js
+++ graficos/common.js
@@ -1,5 +1,12 @@
-const getCSS = (variavel) => {
-    return getComputedStyle(document.body).getPropertyValue(variavel)
+cconst getCSS = (variavel) => {
+    const bodyStyles = getComputedStyle(document.body)
+    return bodyStyles.getPropertyValue(variavel)
 }
 
-export {getCSS}
+const tickConfig = {
+    family: getCSS('--font'),
+    size: 16,
+    color: getCSS('--primary-color')
+}
+
+export {getCSS, tickConfig}
